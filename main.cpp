ab85f684b1e4522b4506c080a12a8bcade889e1b
chanha <you@example.com>
2022-05-28 02:16:53 +0900

    renew testCase

--- main.cpp
+++ main.cpp
@@ -25,22 +25,22 @@ vector<Bin> callSolve(const vector<int>& input)
 
 int main()
 {
-    // vector<int> input = { 4, 10, 0, 1, 4, 5, 13, 15, 10, 11 };
+    vector<int> input = { 4, 10, 0, 1, 4, 5, 13, 15, 10, 11 };
     // vector<int> input1 = { 4, 12, 0, 1, 2, 3, 4, 6, 8, 9, 10, 11, 12, 14 };
     // vector<int> input2 = { 4, 10, 0, 1, 3, 4, 5, 7, 8, 10, 12, 14};
     // vector<int> input3 = { 4, 9, 4, 5, 6, 8, 9, 10, 12, 13, 14 };
     // vector<int> input4 = { 4, 11, 0, 1, 3, 4, 5, 7, 8, 12, 13, 14, 15};
-    vector<int> input5 = { 5, 18, 0, 1, 3, 4, 5, 7, 8, 12, 13, 14, 15, 17, 22, 23, 25, 27, 31 };
+    // vector<int> input5 = { 5, 18, 0, 1, 3, 4, 5, 7, 8, 12, 13, 14, 15, 17, 22, 23, 25, 27, 31 };
     vector<int> input6 = { 5, 24, 0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 17, 20, 21, 22, 23, 25, 26, 27, 30, 31 };
     vector<int> input7 = { 5, 25, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17, 18, 20, 21, 22, 23, 25, 26, 27, 30, 31 };
 
 
-    // callSolve(input);
+    callSolve(input);
     // callSolve(input1);
     // callSolve(input2);
     // callSolve(input3);
     // callSolve(input4);
-    callSolve(input5);
+    // callSolve(input5);
     callSolve(input6);
     callSolve(input7);
     // callSolve(input8);
